07213e7c17f418538bdb8808e4120f2bcb00d50c
جوري <your_email@gmail.com>
2026-09-16 19:22:39 +0300

    fix: update server for vercel deployment

--- server.js
+++ server.js
@@ -79,24 +79,6 @@ if (process.env.NODE_ENV === "production") {
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((user, done) => done(null, user));
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      const user = {
-        id: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value,
-        photo: profile.photos[0].value,
-      };
-      return done(null, user);
-    }
-  )
-);
 
 // بدء تسجيل الدخول
 app.get(
